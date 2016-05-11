473ddef0a6c2f0b6427634c80dde5c2ed810e3b9
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-05-11 17:37:45 +0300

    forgot password

--- apps/library/Class.php
+++ apps/library/Class.php
@@ -1,9 +1,34 @@
 <?php
 namespace Multiple\Library;
-class SomeClass{
+use Phalcon\Di;
+use Multiple\Library\PHPMailer\smtp,
+    Multiple\Library\PHPMailer\PHPMailer as PHPMailer;
+/*[mail]
+host         = 'smtp.yandex.ru'
+username     = bca.bca.bca
+password     = 'BCA22BCA22'
+security     = tls
+port         = 25
+charset      = UTF-8
+email        = bca.bca.bca@yandex.ru
+from         = BCA*/
 
-    static function someMethod(){
-
+class Mailer {
+    public static function get(){
+        $di = DI::getDefault();
+        $cf = $di->get("mail");
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->isHTML(true);
+        $mail->SMTPAuth = true;
+        $mail->Username = $cf['username'];
+        $mail->Password = $cf['password'];
+        //$mail->SMTPDebug = 1;
+        $mail->CharSet = $cf['charset'];
+        $mail->Host = $cf['host'];
+        $mail->Port = $cf['port'];
+        $mail->SMTPSecure = $cf['security'];
+        $mail->SetFrom($cf['email'], $cf['from']);
+        return $mail;
     }
-
 }
